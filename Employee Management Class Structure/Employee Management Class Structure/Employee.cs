3f25c37ee0a32453ba4b66d16b19721dfcb4780c
iferrer1093 <if0999762@otc.edu>
2025-01-30 09:46:25 -0600

    Add files via upload

---
+++ Employee Management Class Structure/Employee Management Class Structure/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management_Class_Structure
+{
+    public class Employee
+    {
+
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public string Department { get; set; }
+        public double BaseSalary { get; set; }
+
+
+        public Employee (string name, int id, string department, double basesalary)
+        {
+            Name = name;
+            ID = id;
+            Department = department;
+            BaseSalary = basesalary;
+        }
+
+        public virtual void CalculatePay()
+        {
+
+        }
+
+        public virtual void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"{Name} is the employee's name, {ID} is the employee's number, {Department} is the employee's department and, {BaseSalary} is their base salary.");
+        }
+         
+
+
+    }
+}
---
+++ Employee Management Class Structure/Employee Management Class Structure/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Employee_Management_Class_Structure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Somebody = new Employee("Somebody", 1, "Education", 14.2);
+            Somebody.DisplayEmployeeDetails();
+            Console.WriteLine(Somebody.Name);
+            
+        }
+    }
+}
