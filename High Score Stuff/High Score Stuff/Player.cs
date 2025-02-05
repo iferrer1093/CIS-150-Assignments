25a927d4664bc6ade497275142f578f2f5c8e000
iferrer1093 <if0999762@otc.edu>
2025-02-04 23:35:05 -0600

    final high scores commit

---
+++ High Score Stuff/High Score Stuff/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Dynamic;
+using System.Numerics;
+
+namespace High_Score_Stuff
+{
+    public class HighScores
+    {
+        private int score;
+        private string initials;
+
+
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+        public string Initials
+        {
+            get { return initials; }
+            set { initials = value; }
+        }
+
+
+        /*public Fart(int score, string initials)
+        {
+            Score = score;
+            Initials = initials;
+        }
+        */
+
+        public HighScores(int score, string initials)
+        {
+            Initials = initials;
+            Score = score;
+        }
+
+        public void PlayerInfo()
+        {
+            Console.WriteLine($"Score is: {Score}, Initials are: {Initials}");
+        }
+
+        //  4. Create a constructor that takes two parameters, initials, and score, and initializes the corresponding properties.
+
+        //Differentiating between private and public stuff is the biggest issue here.
+        //Not crazy hard just step. 3 that seems confusing.
+    }
+}
+
---
+++ High Score Stuff/High Score Stuff/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace High_Score_Stuff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           HighScores player1 = new HighScores(50, "IF");
+
+            player1.PlayerInfo();
+
+        }
+    }
+}
