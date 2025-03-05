94ebf411d5db8f9d31df6ff36f6f656d932cd991
iferrer1093 <if0999762@otc.edu>
2025-03-04 21:04:09 -0600

    Add files via upload
    final commit

---
+++ Module 5 Catch Blocks/Module 5 Catch Blocks/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Module_5_Catch_Blocks
+{
+    using System;
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = "32";
+
+            try
+            {
+                int age = int.Parse(input);
+                Console.WriteLine($"You are {age} years old.");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The age you entered isn't a valid number format.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+        }
+    }
+}
